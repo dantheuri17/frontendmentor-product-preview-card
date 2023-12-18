554823dffb63429522c321aec08383af23e91660
Daniel Theuri <dantheuri17@gmail.com>
2023-12-18 19:48:41 +0300

    first-changes

--- src/components/PreviewCard.tsx
+++ src/components/PreviewCard.tsx
@@ -1,10 +1,11 @@
 import perfumeImage from "../assets/images/image-product-desktop.jpg";
+import cartIconSVG from "../assets/images/icon-cart.svg"
 
 const PreviewCard = () => {
 	return (
 		<>
 			<div className="flex items-center justify-center w-screen h-screen bg-cream card-container">
-				<div className="card grid w-[38rem] grid-cols-1 lg:grid-cols-2 lg:w-[38rem]">
+				<div className="card grid w-[38rem] grid-cols-1 md:grid-cols-2 lg:grid-cols-2 lg:w-[38rem]">
 					<div className="h-full overflow-hidden image-container rounded-l-2xl">
 						<img
 							src={perfumeImage}
@@ -14,11 +15,11 @@ const PreviewCard = () => {
 					</div>
 
 					<div className="flex flex-col justify-between w-full h-full px-10 py-8 bg-white rounded-r-2xl">
-						<p className="tracking-[.3rem] font-montserrat text-dark-grayish-blue">
+						<p className="tracking-[.3rem] font-montserrat text-dark-grayish-blue text-xs">
 							PERFUME
 						</p>
-						<h3 className="text-4xl tracking-tight font-fraunces">
-							Gabrielle Essence Eau De Parfum
+						<h3 className="text-3xl leading-tight tracking-tight font-fraunces">
+							Gabrielle <br />Essence Eau <br />De Parfum
 						</h3>
 						<p className="text-sm tracking-wider text-dark-grayish-blue">
 							A floral, solar and voluptuous interpretation composed by Olivier
@@ -27,12 +28,21 @@ const PreviewCard = () => {
 						<div className="flex items-center">
 							<p className="text-3xl text-dark-cyan font-fraunces">$149.99</p>
 
-							<p className="ml-5 line-through text-dark-grayish-blue">$169.99</p>
+							<p className="ml-5 text-sm line-through text-dark-grayish-blue">
+								$169.99
+							</p>
 						</div>
 
 						<div className="flex justify-center">
-							<button className="flex justify-center w-full py-3 text-white rounded-lg bg-dark-cyan">
-								Add to Cart
+							<button className="flex items-center justify-around w-full py-3 text-white rounded-lg bg-dark-cyan">
+								<span className="flex">
+									<img
+										src={cartIconSVG}
+										className="object-contain pr-3"
+										alt=""
+									/>
+									<p className="font-bold">Add to Cart</p>
+								</span>
 							</button>
 						</div>
 
